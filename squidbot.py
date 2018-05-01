04a0d587b8271a3b57ae22b546876b619c46e0a4
mkerkel <michael.coles1995@gmail.com>
2018-05-01 21:53:33 +1000

    added mute functions

--- squidbot.py
+++ squidbot.py
@@ -1,7 +1,8 @@
 import discord
 import logging
-import utility as util
 from discord.ext import commands
+from pref import key
+from pref import mute_role
 
 # Error Logging
 logger = logging.getLogger("discord")
@@ -31,6 +32,52 @@ async def goss(ctx):
                   "Roles: {1}".format(member, user_roles))
 
 
+# Muting users
+@bot.command(pass_context=True)
+@commands.has_permissions(manage_roles=True)
+async def mute(ctx, user_name: discord.User):
+    """!mute @user to mute the user"""
+    for server_role in ctx.message.server.roles:
+        if server_role.name == mute_role:
+            mute_role_id = server_role
+    await bot.add_roles(user_name, mute_role_id)
+    await bot.say(str(user_name) + " has been muted!")
+
+
+@mute.error
+async def mute_error(error, ctx):
+    print("Mute error: " + type(error).__name__)
+    if isinstance(error, commands.BadArgument):
+        await bot.say("Unknown user!")
+    elif isinstance(error, commands.CheckFailure):
+        await bot.say("You don't have permission to do that!")
+    elif isinstance(error, commands.CommandInvokeError):
+        await bot.say("I don't have permission to do that!")
+
+
+# Unmute users
+@bot.command(pass_context=True)
+@commands.has_permissions(manage_roles=True)
+async def unmute(ctx, user_name: discord.User):
+    """!unmute @user to unmute the user"""
+    for server_role in ctx.message.server.roles:
+        if server_role.name == mute_role:
+            mute_role_id = server_role
+    await bot.remove_roles(user_name, mute_role_id)
+    await bot.say(str(user_name) + " has been unmuted! Be good " + str(user_name) + "!")
+
+
+@unmute.error
+async def unmute_error(error, ctx):
+    print("Unmute error: " + type(error).__name__)
+    if isinstance(error, commands.BadArgument):
+        await bot.say("Unknown user!")
+    elif isinstance(error, commands.CheckFailure):
+        await bot.say("You don't have permission to do that!")
+    elif isinstance(error, commands.CommandInvokeError):
+        await bot.say("I don't have permission to do that!")
+
+
 # Kicking users
 @bot.command(pass_context=True)
 @commands.has_permissions(kick_members=True)
@@ -42,11 +89,13 @@ async def kick(ctx, user_name: discord.User):
 
 @kick.error
 async def kick_error(error, ctx):
-    print(type(error).__name__)
+    print("Kick error: " + type(error).__name__)
     if isinstance(error, commands.BadArgument):
         await bot.say("Unknown user!")
     elif isinstance(error, commands.CheckFailure):
         await bot.say("You don't have permission to do that!")
+    elif isinstance(error, commands.CommandInvokeError):
+        await bot.say("I don't have permission to do that!")
 
 
 # Banning users
@@ -60,15 +109,16 @@ async def ban(ctx, user_name: discord.User):
 
 @ban.error
 async def ban_error(error, ctx):
-    print(type(error).__name__)
+    print("Ban error: " + type(error).__name__)
     if isinstance(error, commands.BadArgument):
         await bot.say("Unknown user!")
     elif isinstance(error, commands.CheckFailure):
         await bot.say("You don't have permission to do that!")
+    elif isinstance(error, commands.CommandInvokeError):
+        await bot.say("I don't have permission to do that!")
 
 
 # Features to implement:
-# Mute
 # Log server activity
 
-bot.run()
+bot.run(key)
